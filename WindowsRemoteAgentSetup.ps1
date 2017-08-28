56a3872cd6bd118a8c3c913d6f7762dac5be0c66
David Lejeune <dl@qbmt.be>
2017-08-28 11:28:11 +0200

    Windows version finished

--- WindowsRemoteAgentSetup.ps1
+++ WindowsRemoteAgentSetup.ps1
@@ -1,6 +1,9 @@
 
-#Write-Host "Path : $($PSScriptRoot)"
-
+# ////////////////////////////////////////////////////////
+# VARIABLES
+# ////////////////////////////////////////////////////////
+$PythonPath = "C:\Python27"
+$urlJDK="http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-windows-i586.exe"
 
 
 # ////////////////////////////////////////////////////////
@@ -46,7 +49,7 @@ function Show-Header()
 $Host.UI.RawUI.BackgroundColor = ($bckgrnd = 'black')
 $Host.UI.RawUI.ForegroundColor = 'White'
 $Host.PrivateData.ErrorForegroundColor = 'Red'
-$Host.PrivateData.ErrorBackgroundColor = $bckgrnd
+$Host.PrivateData.ErrorBackgroundColor = 'DarkGreen'
 $Host.PrivateData.WarningForegroundColor = 'Magenta'
 $Host.PrivateData.WarningBackgroundColor = $bckgrnd
 $Host.PrivateData.DebugForegroundColor = 'Yellow'
@@ -99,14 +102,6 @@ Else
 
   write-host "Creating the Env Variable $($strEnvVariable) "
   cscript //nologo "$($PSScriptRoot)\CreateEnvVariable.vbs" $strEnvVariable $ApacheFolder
-  #If (Test-Path env:$strEnvVariable)
-  #{
-  #  write-host "Env Variable $($strEnvVariable) exists"
-  #}
-  #Else
-  #{
-  #  write-host "Env Variable $($strEnvVariable) does NOT exist" -ForegroundColor Red;
-  #}
 }
 Write-Host "-------------------------------------------------------------------------------------------"
 
@@ -133,12 +128,13 @@ write-host "**********" -ForegroundColor Blue;
 write-host "* PYTHON *" -ForegroundColor Blue;
 write-host "**********" -ForegroundColor Blue;
 
-
+$boolPythonInstalled="False"
 $strEnvVariable="PYTHON_HOME"
 write-host "Testing the following env variable : $($strEnvVariable) " -ForegroundColor DarkGreen;
 If (Test-Path env:$strEnvVariable)
 {
   write-host "Env Variable $($strEnvVariable) exists"
+  $boolPythonInstalled="True"
 }
 Else
 {
@@ -157,6 +153,7 @@ Else
     {
         "Y"
           {
+            $boolPythonInstalled="True"
             Write-host "Removing the downloaded file" -ForegroundColor DarkGray;
             Remove-Item $output -ErrorAction Ignore
 
@@ -166,8 +163,8 @@ Else
 
             write-host "Adding Env Variable $($strEnvVariable) to PATH "
             $env:Path += ";%PYTHON_HOME%\;%PYTHON_HOME%\Scripts\ "
-            Write-Host "Showing all Path variables : " -ForegroundColor Yellow;
-            Write-Host $env:path.split(";") -ForegroundColor Yellow;
+            Write-Host "Showing all Path variables : " -ForegroundColor DarkGreen;
+            Write-Host ($env:Path).Replace(';',"`n") -ForegroundColor Yellow;
           }
         "N"
           {
@@ -177,8 +174,229 @@ Else
               Write-host "Removing the downloaded file" -ForegroundColor DarkGray;
               Remove-Item $output -ErrorAction Ignore
 
+          }
+    }
+}
+Write-Host "-------------------------------------------------------------------------------------------"
+
+
+  write-host "*******" -ForegroundColor Blue;
+  write-host "* PIP *" -ForegroundColor Blue;
+  write-host "*******" -ForegroundColor Blue;
+
+If (($boolPythonInstalled -eq "True") -and (!(Test-Path "C:\Python27\Scripts\pip.exe")))
+{
+
+
+  $url = "https://bootstrap.pypa.io/get-pip.py"
+  write-host "Downloading from url $($url)" -ForegroundColor Magenta;
+  Start-Sleep -m 100
+  $output = "C:\get-pip.py"
+  $start_time = Get-Date
+  Invoke-WebRequest -Uri $url -OutFile $output
+  Write-host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" -ForegroundColor Magenta;
+
+
+  write-host "Running the get-pip.py script" -ForegroundColor Green;
+  C:\Python27\python.exe C:\get-pip.py
+
+
+
+}
+Else{
+
+  write-host "Pip has been located";
+}
+
+  Write-Host "-------------------------------------------------------------------------------------------"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+write-host "*******" -ForegroundColor Blue;
+write-host "* GIT *" -ForegroundColor Blue;
+write-host "*******" -ForegroundColor Blue;
+
+
+$strEnvVariable=$env:PATH
+write-host "Locating GIT in the PATH enviroment variable " -ForegroundColor DarkGreen;
+
+$foundGitInPath = $strEnvVariable.Contains("C:\Program Files\Git")
+If ($foundGitInPath)
+{
+  write-host "GIT exists"
+}
+Else
+{
+  write-host "GIT does NOT exist" -ForegroundColor Red;
+
+  $url = "https://github.com/git-for-windows/git/releases/download/v2.14.1.windows.1/Git-2.14.1-32-bit.exe"
+  write-host "Downloading from url $($url)" -ForegroundColor Magenta;
+  Start-Sleep -m 100
+  $output = "$($env:USERPROFILE)\git.exe"
+  $start_time = Get-Date
+  Invoke-WebRequest -Uri $url -OutFile $output
+  Write-host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" -ForegroundColor Magenta;
+  Start-Process $output
+  $Result = Read-Host -Prompt 'Did you succesfully complete the GIT Installation wizard?  (y/n) ';
+  switch ($Result.ToUpper())
+    {
+        "Y"
+          {
+            $boolPythonInstalled="True"
+            Write-host "Removing the downloaded file" -ForegroundColor DarkGray;
+            Remove-Item $output -ErrorAction Ignore
+
+          }
+        "N"
+          {
+              Write-Host "Aborting the GIT part of this setup " -ForegroundColor Red;
+              Write-Host "Note : all build plans for bamboo will work correctly (no source code checkout possible)" -ForegroundColor Red;
+
+              Write-host "Removing the downloaded file" -ForegroundColor DarkGray;
+              Remove-Item $output -ErrorAction Ignore
 
           }
     }
 }
+Write-Host "-------------------------------------------------------------------------------------------"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+write-host "********" -ForegroundColor Blue;
+write-host "* JAVA *" -ForegroundColor Blue;
+write-host "********" -ForegroundColor Blue;
+
+
+$strEnvVariable="JAVA_HOME"
+write-host "Testing the following env variable : $($strEnvVariable) " -ForegroundColor DarkGreen;
+If (Test-Path env:$strEnvVariable)
+{
+  write-host "Env Variable $($strEnvVariable) exists"
+}
+Else
+{
+  write-host "Env Variable $($strEnvVariable) does NOT exist" -ForegroundColor Red;
+
+  $url = "http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-windows-i586.exe"
+  write-host "Downloading from url $($url)" -ForegroundColor Magenta;
+  Start-Sleep -m 100
+  $output = "$($env:USERPROFILE)\JDK.exe"
+  $start_time = Get-Date
+  Invoke-WebRequest -Uri $url -OutFile $output
+  Write-host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" -ForegroundColor Magenta;
+  Start-Process $output
+  $Result = Read-Host -Prompt 'Did you succesfully complete the GIT Installation wizard?  (y/n) ';
+  switch ($Result.ToUpper())
+    {
+        "Y"
+          {
+            $boolPythonInstalled="True"
+            Write-host "Removing the downloaded file" -ForegroundColor DarkGray;
+            Remove-Item $output -ErrorAction Ignore
+
+            write-host "The intaller should have taken care of creating the env variable : $($strEnvVariable) "
+          }
+        "N"
+          {
+              Write-Host "Aborting the GIT part of this setup " -ForegroundColor Red;
+              Write-Host "Note : all build plans for bamboo will work correctly (no source code checkout possible)" -ForegroundColor Red;
+
+              Write-host "Removing the downloaded file" -ForegroundColor DarkGray;
+              Remove-Item $output -ErrorAction Ignore
+
+          }
+    }
+
+
+  #write-host "Creating the Env Variable $($strEnvVariable) "
+  #cscript //nologo "$($PSScriptRoot)\CreateEnvVariable.vbs" $strEnvVariable $ApacheFolder
+}
+Write-Host "-------------------------------------------------------------------------------------------"
+
+
+write-host "**********" -ForegroundColor Blue;
+write-host "* BAMBOO *" -ForegroundColor Blue;
+write-host "**********" -ForegroundColor Blue;
+
+
+$strBambooHome="C:\bamboo-agent-home"
+write-host "Testing the bamboo home directory : $($strBambooHome) " -ForegroundColor DarkGreen;
+If (Test-Path $strBambooHome)
+{
+  write-host "Bamboo home directory $($strBambooHome) exists"
+}
+Else
+{
+
+    write-host "Bamboo home directory $($strBambooHome) does NOT exist" -ForegroundColor Red;
+    mkdir $strBambooHome
+    write-host ""
+    write-host "Bamboo home directory $($strBambooHome) has been created" -ForegroundColor Yellow;
+}
+
+$url = "http://84.199.251.203/agentServer/agentInstaller/atlassian-bamboo-agent-installer-6.0.2.jar"
+write-host "Downloading the remote agent from from url $($url)" -ForegroundColor Magenta;
+Start-Sleep -m 100
+$output = "$($env:USERPROFILE)\atlassian-bamboo-agent-installer-6.0.2.jar"
+$start_time = Get-Date
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" -ForegroundColor Magenta;
+
+write-host "Running the agent service jar file" -ForegroundColor Yellow;
+java -jar $output http://84.199.251.203/agentServer/
+
+
+
 Write-Host "-------------------------------------------------------------------------------------------"
